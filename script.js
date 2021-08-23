c478d749d1409be899af0d4dedb8ae7e95264596
DESKTOP-AASTOLS\Hossa <hk2nasr@gmail.com>
2021-08-23 17:51:24 +0200

    added an ID for each button, name and priotiy to change to edit view and normal view

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//"use strict";
+"use strict";
 let tasks = [];
 
 const getPriorityName = function (priority) {
@@ -18,6 +18,37 @@ const deleteTask = function (i) {
   if (!confirm("Are you sure ?")) return;
   tasks.splice(i, 1);
   renderTable();
+};
+const cancelTask = function () {
+  renderTable();
+};
+const saveTask = function (i) {
+  let taskName = document.querySelector(`#nameM${i}`).value;
+  let priority = document.querySelector(`#priorityM${i}`).value;
+  if (taskName !== "" && priority > 0) {
+    tasks[i].name=taskName;
+    tasks[i].priority=priority
+  }
+  renderTable();
+};
+const editTask = function (i) {
+  let task = document.querySelector(`#name${i}`);
+  let priority = document.querySelector(`#priority${i}`);
+  let save=document.querySelector(`#save${i}`)
+  let cancel =document.querySelector(`#cancel${i}`)
+  let edit =document.querySelector(`#edit${i}`)
+  let name=task.innerHTML;
+  task.innerHTML=`<input type="text" id="nameM${i}" class="form-control" value="${name}" />`;
+  priority.innerHTML=`
+    <select id="priorityM${i}" class="form-control">
+    <option value="1">High</option>
+    <option value="2">Medium</option>
+    <option value="3">Low</option>
+  </select>
+`
+ save.setAttribute("style","");
+ cancel.setAttribute("style","");
+ edit.setAttribute("style","display:none;");
 };
 const moveUp = function (i) {
   if (i == 0) return;
@@ -41,8 +72,8 @@ const renderTable = function () {
     const row = `
         <tr>
         <td>${i + 1}</td>
-        <td>${t.name}</td>
-        <td>${getPriorityName(t.priority)}</td>
+        <td id=name${i}>${t.name}</td>
+        <td id=priority${i}>${getPriorityName(t.priority)}</td>
         <td>
         ${
           i > 0
@@ -56,9 +87,9 @@ const renderTable = function () {
         }
         </td>
         <td>
-        <button class="btn btn-primary btn-sm">Edit</button>
-        <button class="btn btn-success btn-sm" style="display:none;">Save</button>
-        <button class="btn btn-danger btn-sm" style="display:none;">Cancel</button>
+        <button class="btn btn-primary btn-sm" id=edit${i} onclick="editTask(${i})">Edit</button>
+        <button class="btn btn-success btn-sm" id=save${i} onclick="saveTask(${i})" style="display:none;">Save</button>
+        <button class="btn btn-danger btn-sm" id=cancel${i} onclick="cancelTask()" style="display:none;">Cancel</button>
         <button class="btn btn-danger btn-sm" onclick="deleteTask(${i})">Delete</button></td>
         </tr>
         `;
@@ -79,30 +110,4 @@ const addTask = function () {
 };
 
 document.querySelector("#add").addEventListener("click", addTask);
-var name = "Test3";
-var age = 22;
-const calcFunction = () => {
-  console.log(this);
-  console.log(`My name is ${this.name} I'm ${this.age} years old`);
-};
-const obj = {
-  name: "Test",
-  age: 35,
-  cal: calcFunction,
-};
 
-const obj2 = {
-  name: "Test2",
-  age: 22,
-  cal: calcFunction,
-};
-
-function thisTest() {
-  let obj1 = "Ramy";
-  var obj2 = "Ahmed";
-  console.log(this);
-  const x = () => {
-    console.log(this);
-  };
-  x();
-}
